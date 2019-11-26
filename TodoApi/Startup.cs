a92b89155b7e99f30887c28c80f84ce15e063379
aloebs29 <andrew.loebs@gmail.com>
2019-11-25 23:01:18 -0500

    Setup postgres database.

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -20,8 +20,11 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddDbContext<TodoContext>(opt =>
+            //    opt.UseInMemoryDatabase("TodoList"));
             services.AddDbContext<TodoContext>(opt =>
-               opt.UseInMemoryDatabase("TodoList"));
+                opt.UseNpgsql(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
